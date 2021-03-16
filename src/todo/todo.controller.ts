8e0e1a3f21099b4031214130554c07b331faf05a
alvalle09 <phredvalle@gmail.com>
2021-03-16 00:19:40 -0500

    add @Delete directive

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   ParseIntPipe,
   Put,
+  Delete,
 } from '@nestjs/common';
 import { Todo } from './todo.interface';
 import { TodoService } from './todo.service';
@@ -22,25 +23,31 @@ export class TodoController {
 
   @Post()
   create(@Body() todo: Todo): void {
-    this.logger.log('Handling create() request...');
+    this.logger.log('Handling create request...');
     return this.todoService.create(todo);
   }
 
   @Get()
   findAll(): Todo[] {
-    this.logger.log('Handling findAll() request...');
+    this.logger.log('Handling findAll request...');
     return this.todoService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Todo {
-    this.logger.log('Handling findOne() request with id=' + id + '....');
+    this.logger.log('Handling findOne request with id=' + id + '....');
     return this.todoService.findOne(id);
   }
 
-  @Put(':id'_)
+  @Put(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() todo: Todo): void {
-    this.logger.log('Handling update() request with id= ' + id + '...');
+    this.logger.log('Handling update request with id= ' + id + '...');
     return this.todoService.update(id, todo);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): void {
+    this.logger.log('Handling remove request with id=' + id +'...');
+    return this.todoService.remove(id);
   }
 }
